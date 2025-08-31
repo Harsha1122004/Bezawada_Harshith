a58e3c0f2823143d1ca8a00f374351a9378ac72f
harshith bezawada <221fa04424@gmail.com>
2025-08-31 12:37:55 +0530

    feat: updated in routes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -16,7 +16,7 @@ const projects = [
       "A centralized platform for managing PhD applications, research progress, and academic collaboration efficiently.",
     image: portalImage, // Use the imported image variable
     liveLink: "#",
-    githubLink: "https://github.com/Harsha1122004/PhD-Student-Portal",
+    githubLink: "https://phdportal.vercel.app/",
   },
   {
     id: 2,
